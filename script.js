24ef63b995f770e73f064c12cdb1db2aba0b5f31
akshatk806 <akshatk806@gmail.com>
2023-03-22 19:44:44 +0530

    Changing style and adding author name of qoute

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 fetch('https://type.fit/api/quotes')
     .then(data => data.json())
     .then(joshquotes => {
-        
+
         //for random length of the array deepak
         const randomIndex = Math.floor(Math.random() * joshquotes.length);
         let n
         const quotes = joshquotes[randomIndex].text;
         const joshquotescontent = document.getElementById("joshquotescontent");
         joshquotescontent.innerHTML = quotes;
+
+        const author = joshquotes[randomIndex].author;
+        const authorName = document.getElementById("author");
+        authorName.innerHTML = author;
 })
